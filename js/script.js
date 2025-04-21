90682e7c88ce6728a57e3f0916905ed556994967
diasha07 <dianaschander07@gmail.com>
2025-04-21 23:14:10 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ function perehod(){
     
     p=window.localStorage.getItem('perehod')
     if(p==1){
-        window.open('../html/profile.html')
+        window.open('html/profile.html')
     }
     else{
-        window.open('../html/log.html')
+        window.open('html/log.html')
     }
 }
